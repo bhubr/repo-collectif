0e1eac3d5955b962ca0452f0a5f99f5a76969558
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2018-03-13 11:56:40 +0100

    Add Benoît's profile

---
+++ profiles.js
@@ -0,0 +1,11 @@
+const benoitsProfile = {
+  firstName: 'Benoît',
+  lastName: 'Hubert',
+  age: 39,
+  email: 'benoit.hubert2@free.fr',
+  linkedIn: 'https://www.linkedin.com/in/benoithubertfr/'
+}
+
+const profiles = [
+  benoitsProfile
+]
