79bcfc3d9c45a47ee2fc4d8a620e23da58ab1fae
Luke Skywilder <hohndifool78@free.fr>
2018-03-13 12:04:40 +0100

    wwwilder's profile

--- profiles.js
+++ profiles.js
@@ -1,2 +1,12 @@
-const profiles = []
+const wwwildersProfile = {
+  firstName: 'Waldo',
+  lastName: 'Wilder',
+  age: 99,
+  email: 'noreply@example.com',
+  linkedIn: ''
+}
+
+const profiles = [
+  wwwildersProfile
+]
 
